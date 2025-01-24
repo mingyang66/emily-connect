aa11303bd6f1c13d5883df177b190df1e412c367
yaomingyang <mingyang@eastmoney.com>
2025-01-24 11:14:40 +0800

    optimize code

--- sample-connect-server/src/main/java/com/emily/connect/sample/server/plugin/RequestJsonPlugin.java
+++ sample-connect-server/src/main/java/com/emily/connect/sample/server/plugin/RequestJsonPlugin.java
@@ -10,7 +10,6 @@ import com.emily.infrastructure.json.JsonUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -39,45 +38,6 @@ public class RequestJsonPlugin implements Plugin<String> {
         this.handlerMapping = handlerMapping;
     }
 
-    // 检查类型是否是原始类型或包装类型
-    public static boolean isWrapper(Class<?> type) {
-        if (Byte.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Short.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Integer.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Long.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Float.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Double.class.isAssignableFrom(type)) {
-            return true;
-        } else if (Boolean.class.isAssignableFrom(type)) {
-            return true;
-        } else return Character.class.isAssignableFrom(type);
-    }
-
-    public static Object toPrimitive(String value, Class<?> type) {
-        if (type == byte.class) {
-            return StringUtils.isBlank(value) ? (byte) 0 : Byte.parseByte(value);
-        } else if (type == short.class) {
-            return StringUtils.isBlank(value) ? (short) 0 : Short.parseShort(value);
-        } else if (type == int.class) {
-            return StringUtils.isBlank(value) ? 0 : Integer.parseInt(value);
-        } else if (type == long.class) {
-            return StringUtils.isBlank(value) ? 0L : Long.parseLong(value);
-        } else if (type == float.class) {
-            return StringUtils.isBlank(value) ? 0.0f : Float.parseFloat(value);
-        } else if (type == double.class) {
-            return StringUtils.isBlank(value) ? 0.0d : Double.parseDouble(value);
-        } else if (type == char.class) {
-            return StringUtils.isBlank(value) ? "\\u0000" : value.charAt(0);
-        } else if (type == boolean.class) {
-            return !StringUtils.isBlank(value) && Boolean.parseBoolean(value);
-        }
-        throw new IllegalArgumentException("Unsupported Parameter Type：" + type.getName());
-    }
 
     @Override
     public boolean supports(PluginType pluginType) {
@@ -122,12 +82,6 @@ public class RequestJsonPlugin implements Plugin<String> {
                         args[i] = response;
                     } else if (String.class.isAssignableFrom(parameterType)) {
                         args[i] = payload[j++].getValue();
-                    } else if (parameterType.isPrimitive()) {
-                        String value = payload[j++].getValue();
-                        args[i] = toPrimitive(value, parameterType);
-                    } else if (isWrapper(parameterType)) {
-                        String value = payload[j++].getValue();
-                        args[i] = value == null ? null : JsonUtils.toJavaBean(value, parameterType);
                     } else {
                         String value = payload[j++].getValue();
                         args[i] = value == null ? null : JsonUtils.toJavaBean(value, parameterType);
